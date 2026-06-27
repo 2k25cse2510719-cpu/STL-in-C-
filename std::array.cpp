715fdc363bdd762d7c38ad7c98e02d76b0ddb8a1
Ishan Chauhan <2k25cse2510719@gmail.com>
2026-06-27 21:52:30 +0530

    std::array.cpp
    This file contains commented-out code demonstrating the use of std::array in C++. It includes examples of array initialization, input/output operations, and member functions.

---
+++ std::array.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// #include<array>//important to include if you want to use array an a container .
+// using namespace std ;
+// int main (){
+//     std::array<int,5> arr;//"arr" is a data member , because it contains data .
+//      cout<<"Enter Elements"<<endl;
+//  for(int i =0;i<arr.size()//can use arr.size() here because std::array is a container// 
+//  ;i++){//this is a data function because it performs function on data member//
+//  cin>>arr[i];
+//  }
+//  cout<<arr.front()<<endl;//prints the first value in the array
+// cout<<arr.back()<<endl;//prints the last value in the array
+// std::array<int ,5>brr={1,2,3,4,5};
+// for(int i=0;i<arr.size();i++){
+//     cout<<arr[i]<<" "<<endl;
+// }
+// brr.swap(arr);//used to swap two whole arrays (could not have done it in an ordinary c array
+// // // ,std::arr is a special c++ array , is very compatible with stl , algorithms & member functions )
+// // //both the arrays need to be of the same size
+//    cout<<arr[i]<<" ";
+//  }
+//  cout<<brr.at(3);
+//  int *p=brr.data();//return then stores (pointer) address of the first element in p.
+//  cout<<p;
+// arr[4]=arr.operator[](4);
+// }
